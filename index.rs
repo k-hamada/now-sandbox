1d20bb948de76662e531efd56adb16a3e9728f42
k.hamada <k.hamada.1988@gmail.com>
2019-03-17 17:50:54 +0900

    show uri.path

--- index.rs
+++ index.rs
@@ -3,5 +3,5 @@ use http::{self, Request, Response, StatusCode};
 fn handler(request: Request<()>) -> http::Result<Response<String>> {
     Response::builder()
         .status(StatusCode::OK)
-        .body("Hello World".to_string())
+        .body(request.uri().path().to_string())
 }
