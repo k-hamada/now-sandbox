193e6f2a49e44e2b31c1d0b53c566fff0330b34f
k.hamada <k.hamada.1988@gmail.com>
2019-03-18 21:52:29 +0900

    cargo.toml-entry-point

--- index.rs
+++
@@ -1,7 +0,0 @@
-use http::{self, Request, Response, StatusCode};
-
-fn handler(request: Request<()>) -> http::Result<Response<String>> {
-    Response::builder()
-        .status(StatusCode::OK)
-        .body(request.uri().path().to_string())
-}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use http::{self, StatusCode};
+use now_lambda::{lambda, IntoResponse, Request, Response};
+use std::error::Error;
+
+fn handler(request: Request) -> Result<impl IntoResponse, http::Error> {
+    match request.uri().path() {
+        "/today" => today(),
+        _ => not_found()
+    }
+}
+
+fn today() -> http::Result<Response<String>> {
+    Response::builder()
+        .status(StatusCode::OK)
+        .body("today".to_string())
+}
+
+fn not_found() -> http::Result<Response<String>> {
+    Response::builder()
+        .status(StatusCode::OK)
+        .body("
+        USAGE
+            GET /<year>/<month>/<day>
+            GET /today
+            GET /json
+        ".to_string())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    Ok(lambda!(handler))
 }
