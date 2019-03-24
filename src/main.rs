6231a70d56f4d4211f164d035578f6f5d0282fb3
k.hamada <k.hamada.1988@gmail.com>
2019-03-24 18:56:13 +0900

    json

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use http::{self, StatusCode};
 use now_lambda::{lambda, IntoResponse, Request, Response};
 use std::error::Error;
+use serde::Serialize;
+
+#[derive(Serialize, Debug)]
+struct JsonToday {
+    today: bool,
+}
 
 fn handler(request: Request) -> Result<impl IntoResponse, http::Error> {
     match request.uri().path() {
         "/today" => today(),
+        "/json" => json(),
         _ => not_found()
     }
 }
@@ -13,6 +20,19 @@ fn today() -> http::Result<Response<String>> {
     Response::builder()
         .status(StatusCode::OK)
         .body("today".to_string())
+}
+
+fn json() -> http::Result<Response<String>> {
+    let value = JsonToday { today: true };
+    let serialized = serde_json::to_string(&value);
+    let (status_code, body) = match serialized {
+        Ok(json) => (StatusCode::OK, json),
+        Err(err) =>(StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+    };
+
+    Response::builder()
+        .status(status_code)
+        .body(body)
 }
 
 fn not_found() -> http::Result<Response<String>> {
