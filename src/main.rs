dea7d628dc8ec3edff2ce6c38dd3a15b7db6494e
k.hamada <k.hamada.1988@gmail.com>
2019-03-24 18:58:37 +0900

    (StatusCode, String)

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,10 @@ struct JsonToday {
 }
 
 fn handler(request: Request) -> Result<impl IntoResponse, http::Error> {
-    match request.uri().path() {
+    let (status_code, body) = match request.uri().path() {
         "/today" => today(),
         "/json" => json(),
         _ => not_found()
-    }
-}
-
-fn today() -> http::Result<Response<String>> {
-    Response::builder()
-        .status(StatusCode::OK)
-        .body("today".to_string())
-}
-
-fn json() -> http::Result<Response<String>> {
-    let value = JsonToday { today: true };
-    let serialized = serde_json::to_string(&value);
-    let (status_code, body) = match serialized {
-        Ok(json) => (StatusCode::OK, json),
-        Err(err) =>(StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
     };
 
     Response::builder()
@@ -35,16 +20,28 @@ fn json() -> http::Result<Response<String>> {
         .body(body)
 }
 
-fn not_found() -> http::Result<Response<String>> {
+fn today() -> (StatusCode, String) {
+    (StatusCode::OK, "today".to_string())
+}
+
+fn json() -> (StatusCode, String) {
+    let value = JsonToday { today: true };
+    let serialized = serde_json::to_string(&value);
+    match serialized {
+        Ok(json) => (StatusCode::OK, json),
+        Err(err) =>(StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+    }
+}
+
+fn not_found() -> (StatusCode, String) {
     let usage = "\
         USAGE\
         \n    GET /<year>/<month>/<day>\
         \n    GET /today\
         \n    GET /json\
     ";
-    Response::builder()
-        .status(StatusCode::OK)
-        .body(usage.to_string())
+
+    (StatusCode::OK, usage.to_string())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
